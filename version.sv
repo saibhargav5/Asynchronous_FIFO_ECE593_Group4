06d6310e72f3a783f1f71e447de41824078b5ee5
mahalsa <126035385+mahalsa@users.noreply.github.com>
2024-02-03 23:28:18 -0800

    Update version.sv

--- version.sv
+++ version.sv
@@ -1,6 +1,6 @@
 # Asynchronous_FIFO_ECE593_Group4
 This repository is created to track the project work of ECE-593-WCC Winter 2024. The project we are going to work on is Design and Verification of Asynchronous FIFO using system verilog and UVM.
-e async_fifo(
+module async_fifo(
 //write interface 
 wr_clk_i,rd_clk_i,rst_i,wdata_i,full_o,wr_en_i,wr_error_o,
 
